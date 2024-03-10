b84b93aec3653d842de6316318a3e7c992388f83
fungleo <web@fengcms.com>
2024-03-10 12:57:48 +0800

    Custom ConnectButton is ok

--- src/components/core/Wallet/ConnectButton.tsx
+++ src/components/core/Wallet/ConnectButton.tsx
@@ -1,7 +1,67 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 const ConnectBtn = () => {
-  return <ConnectButton />
+  return (
+    <ConnectButton.Custom>
+      {({ account, chain, openAccountModal, openChainModal, openConnectModal, authenticationStatus, mounted }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== 'loading'
+        const connected =
+          ready && account && chain && (!authenticationStatus || authenticationStatus === 'authenticated')
+
+        return (
+          <>
+            {(() => {
+              if (!connected) {
+                return (
+                  <button onClick={openConnectModal} type="button">
+                    Connect Wallet
+                  </button>
+                )
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <button onClick={openChainModal} type="button">
+                    Wrong network
+                  </button>
+                )
+              }
+
+              return (
+                <>
+                  <button onClick={openChainModal} style={{ display: 'flex', alignItems: 'center' }} type="button">
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          width: 12,
+                          height: 12,
+                          borderRadius: 999,
+                          overflow: 'hidden',
+                          marginRight: 4
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <img alt={chain.name ?? 'Chain icon'} src={chain.iconUrl} style={{ width: 12, height: 12 }} />
+                        )}
+                      </div>
+                    )}
+                    {chain.name}
+                  </button>
+
+                  <button onClick={openAccountModal} type="button">
+                    {account.displayName}
+                  </button>
+                </>
+              )
+            })()}
+          </>
+        )
+      }}
+    </ConnectButton.Custom>
+  )
 }
 
 export default ConnectBtn
